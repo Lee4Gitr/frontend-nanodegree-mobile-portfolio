1cebc5a1bb820b0c0d200c54b76b071603bd2f0f
Lee4Gitr <Lee4Gitr@gmail.com>
2016-07-11 15:22:20 -0400

    added janky pizzas and a readme.

--- src/views/js/main.js
+++ src/views/js/main.js
@@ -494,3 +494,23 @@ var timeToGenerate = window.performance.getEntriesByName("measure_pizza_generati
 console.log("Time to generate pizzas on load: " + timeToGenerate[0].duration + "ms");
 
 // Removed Janky sliding Pizzas in the background. Even though it was cheesy and in bad taste, it added nothing of value to the website.
+
+// runs updatePositions on scroll
+window.addEventListener('scroll', updatePositions);
+
+// Generates the sliding pizzas when the page loads.
+document.addEventListener('DOMContentLoaded', function() {
+  var cols = 8;
+  var s = 256;
+  for (var i = 0; i < 200; i++) {
+    var elem = document.createElement('img');
+    elem.className = 'mover';
+    elem.src = "images/pizza.png";
+    elem.style.height = "100px";
+    elem.style.width = "73.333px";
+    elem.basicLeft = (i % cols) * s;
+    elem.style.top = (Math.floor(i / cols) * s) + 'px';
+    document.querySelector("#movingPizzas1").appendChild(elem);
+  }
+  updatePositions();
+});
